57cd99fe06119adfa440ddc2e5bc879b874635e6
midoyasser16204 <hamada16204@gmail.com>
2025-11-11 16:51:03 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/constructeye/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.constructeye
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/themes/AppThemes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppThemes{
+  static final ThemeData lightTheme = ThemeData(
+
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+
+  );
+}
