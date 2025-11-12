58f27bc4fab339d0f0168eb6f66b92ad0c969e01
midoyasser16204 <hamada16204@gmail.com>
2025-11-12 11:21:10 +0200

    add dark icons and put it's path in AppConstants.dart

--- lib/core/constants/AppConstants.dart
+++ lib/core/constants/AppConstants.dart
@@ -1,36 +1,64 @@
 class AppConstants {
-  // Icons
+  // -----------------------------
+  // 🧩 General Icons
+  // -----------------------------
   static const String addProjectIcon = 'assets/icons/add_project_ico.svg';
-  static const String alertContainerIcon = 'assets/icons/alert_container_ico.svg';
   static const String blueArrowBackIcon = 'assets/icons/blue_arrow_back_ico.svg';
+  static const String completeIcon = 'assets/icons/complete_ico.svg';
+  static const String homeIcon = 'assets/icons/home_ico.svg';
+  static const String linkIcon = 'assets/icons/link_ico.svg';
+  static const String liveStreamIcon = 'assets/icons/live_stream_ico.svg';
+  static const String locationIcon = 'assets/icons/location_ico.svg';
+  static const String logoIcon = 'assets/icons/logo.svg';
+  static const String mailIcon = 'assets/icons/mail_ico.svg';
+  static const String notificationIcon = 'assets/icons/notification_ico.svg';
+  static const String passwordIcon = 'assets/icons/password_ico.svg';
+  static const String personIcon = 'assets/icons/person_ico.svg';
+  static const String progressIcon = 'assets/icons/progress_ico.svg';
+  static const String reportsIcon = 'assets/icons/reports_ico.svg';
+  static const String saveIcon = 'assets/icons/save_ico.svg';
+  static const String timeIcon = 'assets/icons/time_ico.svg';
+  static const String whiteArrowBackIcon = 'assets/icons/white_arrow_back_ico.svg';
+
+  // -----------------------------
+  // 📦 Light Container Icons
+  // -----------------------------
+  static const String alertContainerIcon = 'assets/icons/alert_container_ico.svg';
   static const String cameraContainerIcon = 'assets/icons/camera_container_ico.svg';
   static const String companyContainerIcon = 'assets/icons/company_container_ico.svg';
   static const String completeContainerIcon = 'assets/icons/complete_container_ico.svg';
-  static const String completeIcon = 'assets/icons/complete_ico.svg';
   static const String downloadContainerIcon = 'assets/icons/download_container_ico.svg';
-  static const String homeIcon = 'assets/icons/home_ico.svg';
-  static const String liveStreamIcon = 'assets/icons/live_stream_ico.svg';
-  static const String logoIcon = 'assets/icons/logo.svg';
   static const String logoutContainerIcon = 'assets/icons/logout_container_ico.svg';
   static const String mailContainerIcon = 'assets/icons/mail_container_ico.svg';
-  static const String mailIcon = 'assets/icons/mail_ico.svg';
   static const String notificationContainerIcon = 'assets/icons/notification_container_ico.svg';
-  static const String notificationIcon = 'assets/icons/notification_ico.svg';
   static const String passwordContainerIcon = 'assets/icons/password_container_ico.svg';
-  static const String passwordIcon = 'assets/icons/password_ico.svg';
-  static const String personIcon = 'assets/icons/person_ico.svg';
   static const String phoneContainerIcon = 'assets/icons/phone_container_ico.svg';
   static const String progressContainerIcon = 'assets/icons/progress_container_ico.svg';
-  static const String progressIcon = 'assets/icons/progress_ico.svg';
-  static const String reportsIcon = 'assets/icons/reports_ico.svg';
   static const String safetyContainerIcon = 'assets/icons/safety_container_ico.svg';
-  static const String saveIcon = 'assets/icons/save_ico.svg';
   static const String themeContainerIcon = 'assets/icons/theme_container_ico.svg';
-  static const String timeIcon = 'assets/icons/time_ico.svg';
   static const String uploadImageContainerIcon = 'assets/icons/upload_image_container_ico.svg';
   static const String waveContainerIcon = 'assets/icons/wave_container_ico.svg';
-  static const String whiteArrowBackIcon = 'assets/icons/white_arrow_back_ico.svg';
 
-  // Images
+  // -----------------------------
+  // 🌑 Dark Container Icons
+  // -----------------------------
+  static const String alertContainerDarkIcon = 'assets/icons/alert_container_dark_ico.svg';
+  static const String companyContainerDarkIcon = 'assets/icons/company_container_dark_ico.svg';
+  static const String completeContainerDarkIcon = 'assets/icons/complete_container_dark_ico.svg';
+  static const String logoutContainerDarkIcon = 'assets/icons/logout_container_dark_ico.svg';
+  static const String mailContainerDarkIcon = 'assets/icons/mail_container_dark_ico.svg';
+  static const String notificationContainerDarkIcon = 'assets/icons/notification_container_dark_ico.svg';
+  static const String passwordContainerDarkIcon = 'assets/icons/password_container_dark_ico.svg';
+  static const String phoneContainerDarkIcon = 'assets/icons/phone_container_dark_ico.svg';
+  static const String progressContainerDarkIcon = 'assets/icons/progress_container_dark_ico.svg';
+  static const String safetyContainerDarkIcon = 'assets/icons/safety_container_dark_ico.svg';
+  static const String themeContainerDarkIcon = 'assets/icons/theme_container_dark_ico.svg';
+  static const String uploadImageContainerDarkIcon = 'assets/icons/upload_image_container_dark_ico.svg';
+  static const String waveContainerDarkIcon = 'assets/icons/wave_container_dark_ico.svg';
+
+  // -----------------------------
+  // 🖼️ Images
+  // -----------------------------
+  static const String backgroundImage = 'assets/images/background.png';
   static const String logoImage = 'assets/images/logo.png';
 }
