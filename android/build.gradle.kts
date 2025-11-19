0830fdba4de2ab6afc2e415232e67dbee8c3c57c
yassinmekky <yassinmekky361@gmail.com>
2025-11-19 18:14:36 +0200

    hello

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -30,3 +30,9 @@ plugins {
     id("com.google.gms.google-services") version "4.4.4" apply false
 
 }
+
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+    }
+}
